731458b4c0715f80c55d3fe3ddada51c78645607
tien1302 <tiennqse150168@fpt.edu.vn>
2022-07-03 10:56:07 +0700

    Done Member

--- Ass02Solution/SalesWinApp/frmMembers.Designer.cs
+++ Ass02Solution/SalesWinApp/frmMembers.Designer.cs
@@ -45,9 +45,10 @@
             this.panel1.Controls.Add(this.btnRemoveMember);
             this.panel1.Controls.Add(this.btnEditMember);
             this.panel1.Controls.Add(this.btnAddMember);
-            this.panel1.Location = new System.Drawing.Point(0, 3);
+            this.panel1.Location = new System.Drawing.Point(0, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(678, 385);
+            this.panel1.Size = new System.Drawing.Size(885, 513);
             this.panel1.TabIndex = 6;
             // 
             // label2
@@ -55,9 +56,9 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Matura MT Script Capitals", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(105, 8);
+            this.label2.Location = new System.Drawing.Point(175, 11);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(460, 64);
+            this.label2.Size = new System.Drawing.Size(569, 79);
             this.label2.TabIndex = 10;
             this.label2.Text = "Manager Members";
             // 
@@ -65,48 +66,55 @@
             // 
             this.dgvManagerMembers.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.dgvManagerMembers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvManagerMembers.Location = new System.Drawing.Point(12, 83);
+            this.dgvManagerMembers.Location = new System.Drawing.Point(14, 111);
+            this.dgvManagerMembers.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dgvManagerMembers.Name = "dgvManagerMembers";
+            this.dgvManagerMembers.RowHeadersWidth = 51;
             this.dgvManagerMembers.RowTemplate.Height = 25;
             this.dgvManagerMembers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvManagerMembers.Size = new System.Drawing.Size(476, 291);
+            this.dgvManagerMembers.Size = new System.Drawing.Size(690, 388);
             this.dgvManagerMembers.TabIndex = 9;
             // 
             // btnRemoveMember
             // 
-            this.btnRemoveMember.Location = new System.Drawing.Point(536, 284);
+            this.btnRemoveMember.Location = new System.Drawing.Point(724, 386);
+            this.btnRemoveMember.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnRemoveMember.Name = "btnRemoveMember";
-            this.btnRemoveMember.Size = new System.Drawing.Size(118, 49);
+            this.btnRemoveMember.Size = new System.Drawing.Size(152, 76);
             this.btnRemoveMember.TabIndex = 8;
             this.btnRemoveMember.Text = "Remove Member";
             this.btnRemoveMember.UseVisualStyleBackColor = true;
+            this.btnRemoveMember.Click += new System.EventHandler(this.btnRemoveMember_Click);
             // 
             // btnEditMember
             // 
-            this.btnEditMember.Location = new System.Drawing.Point(536, 193);
+            this.btnEditMember.Location = new System.Drawing.Point(724, 247);
+            this.btnEditMember.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnEditMember.Name = "btnEditMember";
-            this.btnEditMember.Size = new System.Drawing.Size(118, 49);
+            this.btnEditMember.Size = new System.Drawing.Size(152, 74);
             this.btnEditMember.TabIndex = 7;
             this.btnEditMember.Text = "Edit Member";
             this.btnEditMember.UseVisualStyleBackColor = true;
-            this.btnEditMember.Click += new System.EventHandler(this.btnEditMember_Click_1);
+            this.btnEditMember.Click += new System.EventHandler(this.btnEditMember_Click);
             // 
             // btnAddMember
             // 
-            this.btnAddMember.Location = new System.Drawing.Point(536, 102);
+            this.btnAddMember.Location = new System.Drawing.Point(724, 111);
+            this.btnAddMember.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnAddMember.Name = "btnAddMember";
-            this.btnAddMember.Size = new System.Drawing.Size(118, 49);
+            this.btnAddMember.Size = new System.Drawing.Size(152, 78);
             this.btnAddMember.TabIndex = 6;
             this.btnAddMember.Text = "Add New Member";
             this.btnAddMember.UseVisualStyleBackColor = true;
-            this.btnAddMember.Click += new System.EventHandler(this.btnAddMember_Click_1);
+            this.btnAddMember.Click += new System.EventHandler(this.btnAddMember_Click);
             // 
             // frmMembers
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(678, 388);
+            this.ClientSize = new System.Drawing.Size(888, 517);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmMembers";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Manager Members";
